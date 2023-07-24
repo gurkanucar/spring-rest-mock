b22eb02b51fe255217d3acfbbbefeb66b1eda8ee
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-24 13:28:23 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/springrestmock/SpringRestMockApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.springrestmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gucardev/springrestmock/SpringRestMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springrestmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
