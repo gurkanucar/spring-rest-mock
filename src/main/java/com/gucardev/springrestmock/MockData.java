e25b672c820a229f98ea75ae6f2778396456c659
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-24 16:52:17 +0300

    feat: MockData entity created

---
+++ src/main/java/com/gucardev/springrestmock/MockData.java
@@ -0,0 +1,38 @@
+package com.gucardev.springrestmock;
+
+import java.time.LocalDate;
+import javax.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MockData {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String path;
+
+  @CreationTimestamp private LocalDate created;
+
+  @Enumerated(EnumType.STRING)
+  private ResponseType responseType;
+
+  @Enumerated(EnumType.STRING)
+  private HttpMethod httpMethod;
+
+  public Integer successStatus;
+  public Integer failureStatus;
+
+  @Column(length = 2000)
+  public String successResponse;
+
+  @Column(length = 2000)
+  public String failureResponse;
+}
