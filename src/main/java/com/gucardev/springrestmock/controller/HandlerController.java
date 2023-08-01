b88a955cb995d55c0778a1c2e32357bd28a82035
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-01 08:44:45 +0300

    feat: mock handler endpoint set from application.properties

--- src/main/java/com/gucardev/springrestmock/controller/HandlerController.java
+++ src/main/java/com/gucardev/springrestmock/controller/HandlerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/mock")
+@RequestMapping("${mock.api.handler.endpoint}")
 public class HandlerController {
 
   @RequestMapping("/**")
