90645eb58546d432760b9dff9d667846402fbdda
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-01 08:51:49 +0300

    feat: existsMockDataByPathAndHttpMethod query created

--- src/main/java/com/gucardev/springrestmock/repository/MockDataRepository.java
+++ src/main/java/com/gucardev/springrestmock/repository/MockDataRepository.java
@@ -2,14 +2,14 @@ package com.gucardev.springrestmock.repository;
 
 import com.gucardev.springrestmock.model.HttpMethod;
 import com.gucardev.springrestmock.model.MockData;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface MockDataRepository extends JpaRepository<MockData, Long> {
 
-    Optional<MockData> findMockDataByPathAndHttpMethod(String path, HttpMethod httpMethod);
+  Optional<MockData> findMockDataByPathAndHttpMethod(String path, HttpMethod httpMethod);
 
+  boolean existsMockDataByPathAndHttpMethod(String path, HttpMethod httpMethod);
 }
