e76857ab1479fa0ae87bf1513bfd47ee24f2cb9d
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-02 08:56:55 +0300

    refactor: HandlerController code refactored

--- src/main/java/com/gucardev/springrestmock/controller/HandlerController.java
+++ src/main/java/com/gucardev/springrestmock/controller/HandlerController.java
@@ -1,11 +1,9 @@
 package com.gucardev.springrestmock.controller;
 
-import com.gucardev.springrestmock.model.HttpMethod;
 import com.gucardev.springrestmock.model.MockData;
-import com.gucardev.springrestmock.service.MockDataService;
+import com.gucardev.springrestmock.service.HandlerService;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,24 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${mock.api.handler.endpoint}")
 public class HandlerController {
 
-  @Value("${mock.api.handler.endpoint}")
-  private String handlerEndpoint;
+  private final HandlerService handlerService;
 
-  private final MockDataService mockDataService;
-
-  public HandlerController(MockDataService mockDataService) {
-    this.mockDataService = mockDataService;
+  public HandlerController(HandlerService handlerService) {
+    this.handlerService = handlerService;
   }
 
   @RequestMapping("/**")
   public ResponseEntity<?> handleRequest(HttpServletRequest request) {
-    String requestPath = request.getRequestURI();
-    requestPath = requestPath.replace(handlerEndpoint, "");
-    String requestMethod = request.getMethod();
-    log.info("Request came to path: {} {}", requestPath, requestMethod);
-    MockData mockData =
-        mockDataService.findByPathAndMethod(requestPath, HttpMethod.getFromString(requestMethod));
-
+    MockData mockData = handlerService.getByRequest(request);
     return ResponseEntity.status(mockData.getSuccessStatus()).body(mockData.getSuccessResponse());
   }
 }
