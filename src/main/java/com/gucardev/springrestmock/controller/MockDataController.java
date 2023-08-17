ba9a80baebe155f23ec10415490979d602810de6
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-17 15:27:08 +0300

    feat: base path api created

--- src/main/java/com/gucardev/springrestmock/controller/MockDataController.java
+++ src/main/java/com/gucardev/springrestmock/controller/MockDataController.java
@@ -3,7 +3,9 @@ package com.gucardev.springrestmock.controller;
 import com.gucardev.springrestmock.model.MockData;
 import com.gucardev.springrestmock.service.MockDataService;
 import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,12 @@ public class MockDataController {
 
   public MockDataController(MockDataService mockDataService) {
     this.mockDataService = mockDataService;
+  }
+
+  @GetMapping("/base-path")
+  public ResponseEntity<Map<String, String>> getBasePath(
+      @Value("${mock.api.handler.endpoint}") String basePath) {
+    return ResponseEntity.ok(Map.of("path", basePath));
   }
 
   @GetMapping("/{id}")
